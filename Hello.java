ce605f9b8e68ee4b0472ebcd52b186b097166b4d
kanishk-21 <70973459+kanishk-21@users.noreply.github.com>
2020-09-09 00:10:07 +0530

    Author added

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Demo {
   public static void main(String[] args){
     System.out.println("Hello World!");
     Sytem.out.println("Bye bye!");
+    System.out.println("By Kanishk");
   }
 }
